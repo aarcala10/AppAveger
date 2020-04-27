ffdbbe05b90d4104c3991e5ba8b203b00c97dc2d
Adrián Arcalá <aarcala10@gmail.com>
2020-04-27 22:32:12 +0200

    Initial Commit

---
+++ AvengersApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AvengersApp
+//
+//  Created by Adrian Arcalá Ocón on 27/04/2020.
+//  Copyright © 2020 Adrian Arcalá Ocón. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
